261537c44aa5cddbab206e6224d9b947fc3c1f7c
hien-pham <huuhien@inspectorio.com>
2021-06-01 20:53:18 +0700

    Add Book reducers

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,3 +1,33 @@
 import { createReducer, on, Action, createSelector } from '@ngrx/store';
 import { BookModel, calculateBooksGrossEarnings } from 'src/app/shared/models';
 import { BooksPageActions, BooksApiActions } from 'src/app/books/actions';
+
+export interface State {
+  collection: BookModel[];
+  activeBookId: string | null;
+}
+
+export const initialState: State = {
+  collection: [],
+  activeBookId: null,
+};
+
+export const booksReducer = createReducer(
+  initialState,
+  on(
+    BooksPageActions.enter,
+    BooksPageActions.clearSelectedBook,
+    (state, _) => {
+      return {
+        ...state,
+        activeBookId: null,
+      };
+    },
+  ),
+  on(BooksPageActions.selectBook, (state, action) => {
+    return {
+      ...state,
+      activeBookId: action.bookId,
+    };
+  }),
+);
