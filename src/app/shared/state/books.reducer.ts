f870963d1fa72a4b5f60e923fa222e685ec7af40
hien-pham <huuhien@inspectorio.com>
2021-06-02 21:07:16 +0700

    Add Books API reducers

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -2,6 +2,14 @@ import { createReducer, on, Action, createSelector } from '@ngrx/store';
 import { BookModel, calculateBooksGrossEarnings } from 'src/app/shared/models';
 import { BooksPageActions, BooksApiActions } from 'src/app/books/actions';
 
+const createBook = (books: BookModel[], book: BookModel) => [...books, book];
+
+const updateBook = (books: BookModel[], changes: BookModel) =>
+  books.map(b => (b.id === changes.id ? Object.assign({}, b, changes) : b));
+
+const deleteBook = (books: BookModel[], bookId: string) =>
+  books.filter(b => b.id !== bookId);
+
 export interface State {
   collection: BookModel[];
   activeBookId: string | null;
@@ -14,20 +22,44 @@ export const initialState: State = {
 
 export const booksReducer = createReducer(
   initialState,
-  on(
-    BooksPageActions.enter,
-    BooksPageActions.clearSelectedBook,
-    (state, _) => {
-      return {
-        ...state,
-        activeBookId: null,
-      };
-    },
-  ),
+  on(BooksPageActions.enter, BooksPageActions.clearSelectedBook, (state, _) => {
+    return {
+      ...state,
+      activeBookId: null,
+    };
+  }),
   on(BooksPageActions.selectBook, (state, action) => {
     return {
       ...state,
       activeBookId: action.bookId,
     };
   }),
+  on(BooksApiActions.booksLoadedSuccess, (state: State, action) => {
+    return {
+      ...state,
+      collection: action.books,
+    };
+  }),
+  on(BooksApiActions.bookDeletedSuccess, (state: State, action) => {
+    return {
+      ...state,
+      collection: deleteBook(state.collection, action.bookId),
+    };
+  }),
+  on(BooksApiActions.bookCreatedSuccess, (state: State, action) => {
+    return {
+      ...state,
+      collection: createBook(state.collection, action.book),
+    };
+  }),
+  on(BooksApiActions.bookUpdatedSuccess, (state: State, action) => {
+    return {
+      ...state,
+      collection: updateBook(state.collection, action.book),
+    };
+  }),
 );
+
+export function reducer(state: State | undefined, action: Action) {
+  return booksReducer(state, action);
+}
